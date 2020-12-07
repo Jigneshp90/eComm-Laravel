526db9357534f7cfc951ac75f26a4c86bf301dba
Jay <jaypanchal90@gmail.com>
2020-12-07 23:24:19 +0530

    Created Add to Cart Form

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -13,7 +13,11 @@
     <h4>Product Category: {{$products['category']}}</h4>
     <h4>Product Description: {{$products['description']}}</h4>
     <br><br>
-    <button class="btn btn-success">Add to Cart</button>
+    <form action="/add_to_cart" method="POST">
+    <input type="hidden" name="product_id" value="{{$products['id']}}">
+    @csrf
+      <button class="btn btn-success">Add to Cart</button>
+    </form>
     <br><br>
     <button class="btn btn-primary">Buy Now</button>
     </div>
